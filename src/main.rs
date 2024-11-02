343f59778193af4e613b73630e320708ecd149e3
Hanabusa Masahiro <masa@hanabusa.net>
2024-11-03 00:09:27 +0900

    mv models under tpcc_models sub crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod schema;
-mod tpcc_models;
 
 fn main() {
     let mut conn = tpcc_models::connect("tpc_c.sqlite").unwrap();
-    tpcc_models::prepare(1, &mut conn).unwrap();
+    tpcc_models::prepare(2, &mut conn).unwrap();
 }
