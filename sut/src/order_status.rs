52ad8dfc142eee24331afcd1a8c24029c9f33811
Hanabusa Masahiro <masa@hanabusa.net>
2024-11-08 23:20:04 +0900

    fix: Order-Status when no order found

--- sut/src/order_status.rs
+++ sut/src/order_status.rs
@@ -13,33 +13,39 @@ pub(crate) async fn order_status(
         let customer =
             tpcc_models::Customer::find(warehouse_id, district_id, customer_id, &mut conn)?;
 
-        let (db_order, db_lines) = customer.last_order(&mut conn)?;
+        match customer.last_order(&mut conn) {
+            Ok((db_order, db_lines)) => {
+                // Re-share to response JSON type
+                let lines = db_lines
+                    .iter()
+                    .map(|ol| if_types::OrderLine {
+                        item_id: ol.item_id(),
+                        supply_warehouse_id: ol.supply_warehouse_id(),
+                        quantity: ol.quantity(),
+                        amount: ol.amount(),
+                        delivery_at: ol.delivery_at().map(|t| t.and_utc()),
+                    })
+                    .collect::<Vec<if_types::OrderLine>>();
 
-        // Re-share to response JSON type
-        let lines = db_lines
-            .iter()
-            .map(|ol| if_types::OrderLine {
-                item_id: ol.item_id(),
-                supply_warehouse_id: ol.supply_warehouse_id(),
-                quantity: ol.quantity(),
-                amount: ol.amount(),
-                delivery_at: ol.delivery_at().map(|t| t.and_utc()),
-            })
-            .collect::<Vec<if_types::OrderLine>>();
+                let (warehouse_id, district_id, order_id) = db_order.id();
+                let order = if_types::Order {
+                    warehouse_id,
+                    district_id,
+                    order_id,
+                    entry_at: db_order.entry_at().and_utc(),
+                    carrier_id: db_order.carrier_id(),
+                    lines: lines,
+                };
 
-        let (warehouse_id, district_id, order_id) = db_order.id();
-        let order = if_types::Order {
-            warehouse_id,
-            district_id,
-            order_id,
-            entry_at: db_order.entry_at().and_utc(),
-            carrier_id: db_order.carrier_id(),
-            lines: lines,
-        };
-
-        Ok(axum::Json(if_types::OrderStatusResponse {
-            orders: vec![order],
-        }))
+                Ok(axum::Json(if_types::OrderStatusResponse {
+                    orders: vec![order],
+                }))
+            }
+            Err(tpcc_models::QueryError::NotFound) => {
+                Ok(axum::Json(if_types::OrderStatusResponse { orders: vec![] }))
+            }
+            Err(e) => Err(e)?,
+        }
     })
     .await?
 }
