88db09f62b9fa9f95270411a8fb83126c4274024
Hanabusa Masahiro <masa@hanabusa.net>
2025-03-11 17:20:07 +0900

    chore: Benchmark condition from Lambda event

--- rte/lambda_function.py
+++ rte/lambda_function.py
@@ -3,9 +3,11 @@ import subprocess
 
 def lambda_handler(event, context):
     # Run rte
-    subprocess.run(["./rte", "run", "-d", "30", "-c", "1", "https://npolvz556254fqb3l4vkgu5mqe0iohsf.lambda-url.ap-northeast-1.on.aws/"]),
+    concurrency = event.get('concurrency', 1)
+    duration = event.get('duration', 60)
+    endpoint = event['endpoint']
+    subprocess.run(["./rte", "run", "-d", str(duration), "-c", str(concurrency), endpoint]),
     return {
         'statusCode': 200,
         'body': json.dumps('Finished')
     }
-
